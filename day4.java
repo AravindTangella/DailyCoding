bdf814c43f3f45589f680d57dcc415827f15ba80
AravindTangella <74135674+AravindTangella@users.noreply.github.com>
2022-03-24 11:35:05 +0530

    Update day4.java

--- day4.java
+++ day4.java
@@ -1,4 +1,5 @@
-Problem 1: cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+Problem 1: cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. 
+	   For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
 		Given this implementation of cons:
 			def cons(a, b):
 			    def pair(f):
@@ -45,7 +46,9 @@ Solution :
 		}
 	}
 	
-Problem 2: Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded. For example, the message '111' would give 3, since it could be decoded as 			'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. For example, '001' is not allowed.
+Problem 2: Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded. 
+	   For example, the message '111' would give 3, since it could be decoded as
+           'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. For example, '001' is not allowed.
 
 Solution :
 	class Solution {
