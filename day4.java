c7f52f9944717ee97e23948536df6af5ba0cc0b9
AravindTangella <74135674+AravindTangella@users.noreply.github.com>
2022-03-24 11:33:36 +0530

    Add files via upload

---
+++ day4.java
@@ -0,0 +1,89 @@
+Problem 1: cons(a, b) constructs a pair, and car(pair) and cdr(pair) returns the first and last element of that pair. For example, car(cons(3, 4)) returns 3, and cdr(cons(3, 4)) returns 4.
+		Given this implementation of cons:
+			def cons(a, b):
+			    def pair(f):
+				return f(a, b)
+			    return pair
+		Implement car and cdr.
+	
+Solution :
+	class Pair<U, V> {
+
+		private U x;
+		private V y;
+		
+		Pair(U x, V y) {
+			this.x = x;
+			this.y = y;
+		}
+		
+		//This method returns the first element of passed in pair object
+		U car(Pair<U, V> p) {
+			return p.x;
+		}
+		
+		//This method returns the second element of passed in pair object
+		V cdr(Pair<U, V> p) {
+			return p.y;
+		}
+		
+	}
+	
+	class PairDemo{
+		public static void main(String[] args) {
+			
+			//Instantiating the object pair1
+			Pair<Integer, Double> pair1 = new Pair<Integer,Double>(2,3.456);
+			
+			//Instantiating the object pair2
+			Pair<Integer, Double> pair2 = new Pair<Integer, Double>
+									(3162983, 56787.82398);
+			
+			System.out.println("The first element of pair2 is "+data.car(pair2));
+			System.out.println("The second element of pair2 is "+pair2.cdr(pair2));
+			
+		}
+	}
+	
+Problem 2: Given the mapping a = 1, b = 2, ... z = 26, and an encoded message, count the number of ways it can be decoded. For example, the message '111' would give 3, since it could be decoded as 			'aaa', 'ka', and 'ak'. You can assume that the messages are decodable. For example, '001' is not allowed.
+
+Solution :
+	class Solution {
+	    public int numDecodings(String s) {
+		
+		if(s.length() == 0 || s.charAt(0) == '0')
+		    return 0;
+		if(s.length() == 1)
+		    return 1;
+		int dp[] = new int[s.length()+1];
+		dp[0] = 1;
+		dp[1] = 1;
+		
+		for(int i = 1; i < s.length(); i++) {
+		    
+		    int singleDigit = s.charAt(i) - '0';
+		    int doubleDigit = (s.charAt(i-1) - '0')*10 + singleDigit;
+		    
+		    if(singleDigit > 0)
+		        dp[i+1] += dp[i];
+		    if(doubleDigit >= 10 && doubleDigit <= 26)
+		        dp[i+1] += dp[i-1];
+		}
+		
+		return dp[s.length()];
+	    }
+	    
+	}
+			
+			
+			
+			
+			
+			
+		
+		
+		
+		
+		
+		
+		
