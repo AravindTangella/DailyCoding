c9237f771df9c64d26be27f4933c45efd30c873d
AravindTangella <74135674+AravindTangella@users.noreply.github.com>
2022-06-10 17:02:47 +0530

    Update 10June2022.java

--- 10June2022.java
+++ 10June2022.java
@@ -1,6 +1,32 @@
 Problem 1: Given three 32-bit integers x, y, and b, return x if b is 1 and y if b is 0, using only mathematical or bit operations. 
             You can assume b can only be 1 or 0.
-Solution : 
+            
+Solution : Approach 1:
+
+            import java.util.Scanner;
+
+            public class BitManupulation {
+                public static void main(String[] args) {
+                    Scanner sc = new Scanner(System.in);
+
+                    System.out.println("Enter x");
+                    int x = sc.nextInt();
+
+                    System.out.println("Enter y");
+                    int y = sc.nextInt();
+
+                    System.out.println("Enter b");
+                    int b = sc.nextInt();
+
+                    //when b is 1, (1-b) would be zero then x will be returned and when b is 0, (x*b) would be zero then y is returned
+                    int result = x*b + y*(1-b);
+
+                    System.out.println(result);
+                }
+            }
+                        
+            Approach 2:  
+                        
             import java.util.Scanner;
 
             public class BitManupulation {
