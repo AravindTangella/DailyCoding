4d720befe9ca63c136739bf6e8aa8b31875dcec5
AravindTangella <74135674+AravindTangella@users.noreply.github.com>
2022-06-15 15:13:06 +0530

    Update 15June2022.java

--- 15June2022.java
+++ 15June2022.java
@@ -1,5 +1,14 @@
-Problem : Given an integer n and a list of integers l, write a function that randomly generates a number from 0 to n-1 that isn't in l (uniform).
-  
+Problem : You are given an integer n and an array of unique integers blacklist. Design an algorithm to pick a random integer 
+          in the range [0, n - 1] that is not in blacklist. Any integer that is in the mentioned range and not in blacklist should be equally 
+          likely to be returned.
+
+        Optimize your algorithm such that it minimizes the number of calls to the built-in random function of your language.
+
+        Implement the Solution class:
+
+        Solution(int n, int[] blacklist) Initializes the object with the integer n and the blacklisted integers blacklist.
+        int pick() Returns a random integer in the range [0, n - 1] and not in blacklist.
+
 Solution : class Solution {
 
               Set<Integer> blacklist;
